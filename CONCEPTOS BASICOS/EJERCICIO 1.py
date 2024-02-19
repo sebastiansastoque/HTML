7705cbe332e7c22beb714b27f655060f67f0b768
sebastiansastoque <159549077+sebastiansastoque@users.noreply.github.com>
2024-02-19 12:50:43 -0500

    Add files via upload

---
+++ CONCEPTOS BASICOS/EJERCICIO 1.py
@@ -0,0 +1,8 @@
+#REALIZACION PUNTO DE GRAFICA GEOMETRICA
+base=3 #en cm
+altura=4 #en cm
+
+#CALCULAR LA AREA DEL TRIANGULO
+area = (base * altura) / 2
+#IMPRIMIR RESULTADO FINAL
+print(("Resultado final del triangulo rectangulo es:"), area,"cm^2")
---
+++ CONCEPTOS BASICOS/EJERCICIO 2.py
@@ -0,0 +1,15 @@
+#CALCULAR LAS NOTAS PROMEDIADAS
+nota1=float(input("Ingresa la nota a calificar:"))
+nota2=float(input("Ingresa la nota a calificar:"))
+nota3=float(input("Ingresa la nota a calificar:"))
+
+peso1=0.36
+peso2=1.25
+peso3=2.36
+
+#CALCULAR EL PREMEDIO DE LA NOTA
+prom=(nota1*peso1)+(nota2*peso2)+(nota3*peso3)
+
+#IMPRIMIR EL RESULTADO FINAL
+
+print("Promediar el total del estudiante en al asignatura:",prom)
