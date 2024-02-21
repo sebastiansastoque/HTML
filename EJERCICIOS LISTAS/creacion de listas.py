2324e1eb711687842e062739c9f0484a864e34b5
sebastiansastoque <159549077+sebastiansastoque@users.noreply.github.com>
2024-02-21 09:37:51 -0500

    Add files via upload

---
+++ EJERCICIOS LISTAS/creacion de listas.py
@@ -0,0 +1,17 @@
+#CREACION_LISTAS
+valor_a= 5
+#              0 1 2 3 4 5 
+valor_lista = [4,7,8,9,10,20,30,40,55]
+print(valor_lista[2])
+#Ejercicio_Lista_Texto
+lista_2 = ["salido","juan","estudiante","incap"]
+print(lista_2)
+#Ejercicio_Lista_Aplicando_append
+lista_2.append("hola")
+print(lista_2)
+#Ejercicio_lista_Aplicando_Pop
+lista_2.pop()
+print(lista_2)
+#Ejercicio_Lista_Aplicando_clear
+lista_2.clear()
+print(lista_2)
