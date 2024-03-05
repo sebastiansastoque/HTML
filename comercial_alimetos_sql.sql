a8c5247dac995d29fe410f90d960ff9b3531f0c4
sebastiansastoque <159549077+sebastiansastoque@users.noreply.github.com>
2024-03-05 15:40:00 -0500

    Add files via upload

---
+++ comercial_alimetos_sql.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-03-2024 a las 21:37:17
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `comercial_alimetos_sql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cargo`
+--
+
+CREATE TABLE `cargo` (
+  `cod_cargo` int(11) NOT NULL,
+  `nombre_cargo` varchar(25) NOT NULL,
+  `salario` decimal(10,0) NOT NULL CHECK (`salario` >= 900000)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `cod_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(50) NOT NULL,
+  `observaciones` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` enum('CC','CE','NIT','RUT') DEFAULT NULL,
+  `no_dumento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenini') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `fecha_ingreso` date DEFAULT curdate(),
+  `edad` int(11) NOT NULL,
+  `telefono` varchar(25) NOT NULL,
+  `estado_civil` enum('soltero','casado','divorciado','union libre','viudo') DEFAULT NULL,
+  `tipo_cliente` enum('detallista','mayorista','empresarial') DEFAULT NULL,
+  `activo` varchar(4) NOT NULL,
+  `empleado_cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contratista`
+--
+
+CREATE TABLE `contratista` (
+  `cod_contratista` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` enum('CC','CE','NIT','RUT') DEFAULT NULL,
+  `no_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` varchar(25) NOT NULL,
+  `camion` enum('camioneta','furgon','camion') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `cod_empleado` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `edad` int(11) NOT NULL CHECK (`edad` >= 18),
+  `telefono` varchar(25) NOT NULL,
+  `fecha_ingreso` date DEFAULT curdate(),
+  `estado_civil` enum('soltero','casado','divorciado','union libre','viudo') DEFAULT NULL,
+  `cargo_cod` int(11) NOT NULL,
+  `nivel_estudios` enum('primaria','bachillerato','tecnico','tecnologo','profecianal','otro') DEFAULT NULL,
+  `eps` enum('saniatas','sura','capital salud','nueva eps','compenasar','famiasanar','aliansalud') DEFAULT NULL,
+  `pensiones` enum('fna','porveir','colfondos','proteccion') DEFAULT NULL,
+  `banco` enum('BBVA','davivienda','bancolombia','caja social','popular','las villas') DEFAULT NULL,
+  `activo` varchar(4) NOT NULL DEFAULT 'A'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nomina`
+--
+
+CREATE TABLE `nomina` (
+  `cod_nomina` int(11) NOT NULL,
+  `fecha_nomina` date NOT NULL,
+  `empleado_cod` int(11) NOT NULL,
+  `salario_base` decimal(10,0) NOT NULL DEFAULT 0,
+  `dias_trabajados` int(11) NOT NULL,
+  `salario` decimal(10,0) NOT NULL,
+  `auxilio_transporte` decimal(10,0) NOT NULL DEFAULT 0,
+  `nro_recargo` int(11) DEFAULT 0,
+  `recargo_noche` decimal(10,0) NOT NULL,
+  `comisiones_otros` decimal(10,0) NOT NULL,
+  `total_vengado` decimal(10,0) NOT NULL,
+  `salud` decimal(10,0) NOT NULL,
+  `pension` decimal(10,0) NOT NULL,
+  `prestamos_otros` decimal(10,0) NOT NULL DEFAULT 0,
+  `total_deducido` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `cod_producto` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `valor_compra` decimal(10,0) NOT NULL DEFAULT 0,
+  `valor_venta` decimal(10,0) NOT NULL DEFAULT 0,
+  `existencia` bigint(20) NOT NULL CHECK (`existencia` >= 0),
+  `nro_lote` int(11) NOT NULL,
+  `fecha_fabricacion` date NOT NULL,
+  `fecha_vencimiento` date NOT NULL,
+  `categoria_cod` int(11) NOT NULL,
+  `proveedor_cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `cod_proveedor` int(11) NOT NULL,
+  `razon_social` varchar(50) NOT NULL,
+  `tipo_documento` enum('NIT','RUT','CC') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `telefono` varchar(25) NOT NULL,
+  `e_mail` varchar(50) NOT NULL,
+  `asesor_comercial` varchar(25) NOT NULL,
+  `telefono_asesor` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD PRIMARY KEY (`cod_cargo`),
+  ADD KEY `nombre_cargo` (`nombre_cargo`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`cod_categoria`),
+  ADD KEY `nombre_categoria` (`nombre_categoria`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD KEY `empleado_cod` (`empleado_cod`);
+
+--
+-- Indices de la tabla `contratista`
+--
+ALTER TABLE `contratista`
+  ADD PRIMARY KEY (`cod_contratista`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`cod_empleado`),
+  ADD KEY `nombre1` (`nombre1`),
+  ADD KEY `cargo_cod` (`cargo_cod`);
+
+--
+-- Indices de la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  ADD PRIMARY KEY (`cod_nomina`),
+  ADD KEY `fecha_nomina` (`fecha_nomina`),
+  ADD KEY `empleado_cod` (`empleado_cod`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`cod_producto`),
+  ADD KEY `descripcion` (`descripcion`),
+  ADD KEY `categoria_cod` (`categoria_cod`),
+  ADD KEY `proveedor_cod` (`proveedor_cod`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`cod_proveedor`),
+  ADD KEY `razon_social` (`razon_social`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  MODIFY `cod_cargo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `cod_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contratista`
+--
+ALTER TABLE `contratista`
+  MODIFY `cod_contratista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `cod_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  MODIFY `cod_nomina` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `cod_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `cod_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`empleado_cod`) REFERENCES `empleado` (`cod_empleado`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`cargo_cod`) REFERENCES `cargo` (`cod_cargo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  ADD CONSTRAINT `nomina_ibfk_1` FOREIGN KEY (`empleado_cod`) REFERENCES `empleado` (`cod_empleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria_cod`) REFERENCES `categoria` (`cod_categoria`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`proveedor_cod`) REFERENCES `proveedor` (`cod_proveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
